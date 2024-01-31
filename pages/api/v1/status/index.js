c624aa9cad1ed39d3bc9c4bea5fbdbbb0b7360cb
Ramon Fossa Chiesi <104222191+RamonFossa@users.noreply.github.com>
2024-01-31 01:19:25 +0000

    feat: Add `.env.development` and model `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-export default function status(req, res) {
+import database from "../../../../infra/database.js";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   res.status(200).json({ chave: "são acima da média" });
-  console.log(res.body);
 }
