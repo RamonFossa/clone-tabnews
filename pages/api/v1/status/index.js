63651e290f77dcd36385f35dde8af8c3d900654f
Ramon Fossa Chiesi <104222191+RamonFossa@users.noreply.github.com>
2024-02-02 07:07:59 +0000

    feat: Initing real `/status` implementation and his tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,9 @@
 import database from "infra/database.js";
 
 export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString(); //Formato ISO: ano-mes-diaThora:minuto:segundo.milisegundoZ === "Z" Indica uma timezone, cada para cada letra do alfabeto
+
+  res.status(200).json({
+    updated_at: updatedAt,
+  });
 }
