d1490c615240739950d7a474c77d725b978eb190
Ramon Fossa Chiesi <104222191+RamonFossa@users.noreply.github.com>
2024-02-12 03:56:01 +0000

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  console.log("Credenciais do POSTGRESS: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
     await client.connect();
     const res = await client.query(queryObject);
     return res;
   } catch (err) {
     console.error(err);
+    throw error;
   } finally {
     await client.end();
   }
