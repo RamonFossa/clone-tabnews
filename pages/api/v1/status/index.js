1d3daa6d8f94aaac27a913748562f74cf027ddba
Ramon Fossa Chiesi <104222191+RamonFossa@users.noreply.github.com>
2024-02-14 01:38:47 +0000

    feat: Add `ssl` to `database`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,7 +5,6 @@ export default async function status(req, res) {
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-  console.log("VALOR:", databaseVersionResult.rows[0].server_version);
 
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
